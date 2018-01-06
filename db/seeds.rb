550ec38c94cf267ca0d907225406618262969b2b
jiro.kirameki <firebom.sns@gmail.com>
2018-01-06 00:57:15 +0000

    upgrade

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-Item.create(image_url:'humburg.jpg', title: '牛肉ハンバーグ', content: '国産牛を100%使った牛肉ハンバーグ。牛肉の旨みが際立ちます。')
-Item.create(image_url:'saba.jpg', title: 'さばの塩焼き', content: '鯖を香ばしく焼き上げ、塩で味付けしました。脂が載っています。')
-Item.create(image_url:'hijiki.jpg', title: 'ひじき', content: 'ミネラル豊富なひじき。健康のためにいかがでしょうか？')
-Item.create(image_url:'karaage.jpg', title: '唐揚げ', content: '衣サクサク、中身はジューシーな唐揚げ')
+Item.create(:image_url => "beef-curry.jpg", :title => "ビーフカレー", :content => "牛肉の旨みがカレールーによく合います。")
